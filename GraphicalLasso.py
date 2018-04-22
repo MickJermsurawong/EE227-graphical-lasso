970f20c5d6694680cd7183e5617eab6418fcfecf
Mick Jermsak Jermsurawong <mick.jermsurawong@gmail.com>
2018-04-22 01:08:38 -0700

    feat: implement graphical lasso decoupled from l1-solver

---
+++ GraphicalLasso.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+class GraphicalLasso(object):
+
+    def __init__(self, l1_solver_f):
+
+        self.l1_solver_f = l1_solver_f
+
+    @staticmethod
+    def _partition_blockwise(X, j):
+        """
+            Assume X is symmetric
+        """
+        X_j = X[:, j]
+
+        x_22 = X_j[j]
+
+        selector = np.ones_like(X_j, dtype=np.bool)
+        selector[j] = False
+        x_12 = X_j[selector]
+
+        X_11 = X[selector][:, selector]
+
+        return X_11, x_12, x_22
+
+    @staticmethod
+    def _update_partition(X, x, j):
+        """
+            Symmetric X [nxn]
+            Vector x [n-1]
+            Update x to X to column/row tiling each element except the jth position
+        """
+        X[j, :j] = x[:j]
+        X[j, j + 1:] = x[j:]
+
+        X[:j, j] = x[:j]
+        X[j + 1:, j] = x[j:]
+
+    def fit(self, S, l1_lambda=0.01, verbose=False):
+        assert np.array_equal(S, S.T), "Initial empirical covariance should be symmetric"
+        feature_dim = S.shape[0]
+        W = np.copy(S)
+
+        # Coordinate descent solver
+        l1_solver = self.l1_solver_f(alpha=l1_lambda)
+
+        # Pre-partition S
+        s_12_all = []
+        for j in range(feature_dim):
+            _, S_12, _ = GraphicalLasso._partition_blockwise(S, j)
+            s_12_all.append(S_12)
+
+        def solve_inner(W, j):
+            """
+                Return beta, and estimated w_12
+            """
+            W_11, _, _ = GraphicalLasso._partition_blockwise(W, j)
+            s_12 = s_12_all[j]
+
+            l1_solver.fit(W_11, s_12)
+            beta = l1_solver.coef_
+            w_12_estimated = np.matmul(W_11, beta)
+
+            return beta, w_12_estimated
+
+        # Check convergence
+        all_betas = [np.ones(feature_dim -1) for j in range(feature_dim)]
+        all_w_12s = [np.ones(feature_dim -1) for j in range(feature_dim)]
+
+        count_converge = 0
+        iter_num = 0
+        while True:
+
+            if iter_num % 10 == 0 and verbose is True:
+                print("Iter #", iter_num)
+
+            j = iter_num % feature_dim
+
+            beta, w_12_estimated = solve_inner(W, j)
+            GraphicalLasso._update_partition(W, w_12_estimated, j)  # diagonal is unchanged
+
+            if np.isclose(all_betas[j], beta).all():
+                count_converge += 1
+            else:
+                count_converge = 0
+
+            all_betas[j] = beta
+            all_w_12s[j] = w_12_estimated
+            iter_num += 1
+
+            if count_converge == feature_dim:
+                break
+
+        # Make each column precision matrix
+        Theta = np.zeros_like(W)
+
+        for j in range(feature_dim):
+            beta, w_12_estimated = all_betas[j], all_w_12s[j]
+            w_22 = W[j][j]
+
+            theta_22 = 1.0 / (w_22 - np.matmul(w_12_estimated.T, beta))
+            theta_12 = beta * -1 * theta_22
+            self._update_partition(Theta, theta_12, j)
+            Theta[j][j] = theta_22
+
+        return Theta
---
+++ test/test_graphical_lasso.py
@@ -0,0 +1,80 @@
+from unittest import TestCase
+
+from sklearn import linear_model
+
+from GraphicalLasso import GraphicalLasso as GL
+import numpy as np
+
+
+class TestGraphicalLasso(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestGraphicalLasso, self).__init__(*args, **kwargs)
+        self.standard_a = np.array([
+            [0, 1, 2],
+            [1, 4, 3],
+            [2, 3, 5]
+        ])
+
+    def test_partitioning(self):
+        # Partition at last block
+        A = np.copy(self.standard_a)
+        X_11, x_12, x_22 = GL._partition_blockwise(A, 2)
+        self.assertTrue(np.array_equal(X_11, np.array([[0, 1], [1, 4]])))
+        self.assertTrue(np.array_equal(x_12, np.array([2, 3])))
+        self.assertTrue(x_22 == 5)
+
+        # Partition middle
+        X_11, x_12, x_22 = GL._partition_blockwise(A, 1)
+        self.assertTrue(np.array_equal(X_11, np.array([[0, 2], [2, 5]])))
+        self.assertTrue(np.array_equal(x_12, np.array([1, 3])))
+        self.assertTrue(x_22 == 4)
+
+    def test_update_partition(self):
+
+        A = np.copy(self.standard_a)
+        GL._update_partition(A, np.array([9, 7]), 1)
+        self.assertTrue(np.array_equal(A, np.array([
+            [0, 9, 2],
+            [9, 4, 7],
+            [2, 7, 5]
+        ])))
+
+    def test_fit(self):
+
+        Theta_true = np.array([
+            [2.0, 0.6, 0.0, 0.0, 0.5],
+            [0.6, 2.0, -0.4, 0.3, 0.0],
+            [0.0, -0.4, 2.0, -0.2, 0.0],
+            [0.0, 0.3, -0.2, 2.0, -0.2],
+            [0.5, 0.0, 0.0, -0.2, 2.0]
+        ])
+        feature_dim = Theta_true.shape[0]
+
+        assert np.array_equal(Theta_true.T, Theta_true), "True precision should be symmetric"
+
+        cov = np.linalg.inv(Theta_true)
+        data_set = np.random.multivariate_normal(np.zeros(feature_dim), cov, (20000))
+        S = np.cov(data_set.T)
+        S_inv = np.linalg.inv(S)
+
+        gl = GL(l1_solver_f=linear_model.Lasso)
+
+        theta_estimated = gl.fit(S, l1_lambda=0.0001)
+
+        self.assertEqual(theta_estimated.shape[0], feature_dim)
+
+        print("Estimated: ")
+        print(np.round(theta_estimated, decimals=2))
+
+        print("S-inverse: ")
+        print(np.round(S_inv, decimals=2))
+
+        for i in range(feature_dim):
+            for j in range(feature_dim):
+                self.assertAlmostEqual(theta_estimated[i][j], S_inv[i][j], delta=0.1)
+
+
+
+
+
