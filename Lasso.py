2e1fe50ad560653ba93bd9c0c37a4fe17825c226
leomari <mari.leonhardsen@gmail.com>
2018-05-06 16:54:58 -0700

    Updated Lasso

--- Lasso.py
+++ Lasso.py
@@ -4,7 +4,8 @@ import copy
 
 class Lasso2(object):
     
-    def __init__(self, alpha, max_iter = 1000, fit_intercept = True, tol = 1e-8):
+    def __init__(self, alpha, max_iter = 1000, fit_intercept = True, tol = 1e-50):
+
         self.alpha = alpha 
         self.n_iter_ = None 
         self.coef_ = None 
@@ -15,7 +16,7 @@ class Lasso2(object):
     
     def soft_tresh(self, x, alpha):
         if x < -alpha:
-            return x + alpha 
+            return x + alpha
         elif x > alpha: 
             return x - alpha 
         else:
@@ -23,8 +24,13 @@ class Lasso2(object):
     
     def get_objective(self, X, y, beta):
         n = X.shape[0]
-        obj = (1/2*n)*(LA.norm(y-np.dot(X,beta)))**2 + self.alpha*LA.norm(beta, ord=1)
+        obj = (1/(2*n))*(LA.norm(y-np.dot(X,beta)))**2 + self.alpha*LA.norm(beta, ord=1)
         return obj
+
+    def get_loss(self, X, y, beta):
+        n = X.shape[0]
+        loss = (1/(2*n))*(LA.norm(y-np.dot(X,beta)))**2
+        return loss
     
     def fit(self, X, y):
         p = X.shape[1]
@@ -34,7 +40,6 @@ class Lasso2(object):
             X = np.column_stack((np.ones(n), X))
             p = X.shape[1]
 
-        convergence = False
         beta = np.zeros(p)
         
         if self.fit_intercept: 
@@ -52,14 +57,16 @@ class Lasso2(object):
                 beta_bar[j] = 0
                 r_j = y - np.dot(X, beta_bar)
                 x_j = X[:,j]
-                beta[j] = (1/LA.norm(x_j))**2 * self.soft_tresh(np.dot(r_j, x_j), n*self.alpha)
+                if LA.norm(x_j) == 0:
+                    beta[j] = 0
+                else:
+                    beta[j] = (1/LA.norm(x_j))**2 * self.soft_tresh(np.dot(r_j, x_j), n*self.alpha)
                 if self.fit_intercept: 
                     beta[0] = np.sum(y - np.dot(X[:, 1:], beta[1:]))/n
            
-            #if np.allclose(beta, beta_prev, rtol = 1e-10) and np.isclose(self.get_objective(X,y,beta), self.get_objective(X,y,beta_prev), rtol=1e-10):
-            #if ((abs(beta_prev - beta)<=self.tol).all ()):
-            #    break
-         
+            if  ((abs(beta_prev - beta)<=self.tol).all ()) and np.isclose(self.get_objective(X,y,beta), self.get_objective(X,y,beta_prev), rtol=self.tol):
+                break
+
         self.n_iter_ = i
         if self.fit_intercept:
             self.intercept_ = beta[0]
